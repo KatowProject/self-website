e894cbc14f0c3c8935d90d821c094fa30ba00840
KatowProject <naufalfaqih443@gmail.com>
2021-12-24 09:50:36 +0700

    update

--- index.js
+++ index.js
@@ -1,24 +1,30 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const fetch = require('node-fetch');
 
 app.use(cors());
 app.set('view engine', 'ejs');
-
 app.use(express.static('assets'));
 
-
 app.get('/', (req, res) => {
-    res.render('pages/index');
+    res.render('pages/index', { data: require('./data/projects.json') });
 });
 
-
-app.get('/:query', (req, res) => {
+app.get('/:query', async (req, res) => {
     const query = req.params.query;
 
     switch (query) {
-        case 'komikato':
-            res.render('pages/komikato');
+        case 'projects':
+            const projects = await fetch('https://api.github.com/users/KatowProject/repos');
+            const projectsJSON = await projects.json();
+            const filter = projectsJSON.filter(project => !project.fork && project.language !== null);
+
+            res.render('pages/project', { data: filter });
+            break;
+
+        case 'about':
+            res.render('pages/about');
             break;
 
         default:
@@ -31,4 +37,5 @@ app.get('/:query', (req, res) => {
 app.get('*', (req, res) => {
     res.render('pages/404');
 });
+
 app.listen(9000, () => console.log('Server started on port 9000'));
